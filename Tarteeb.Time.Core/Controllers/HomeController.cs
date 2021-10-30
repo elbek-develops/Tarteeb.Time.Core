146cd550971c8c37ec55bcb7bc284284d3a25f28
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-10-30 06:23:13 -0700

    Add sample controller

---
+++ Tarteeb.Time.Core/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Tarteeb.Time.Core.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => Ok("Time microservice is up and running!");
+    }
+}
